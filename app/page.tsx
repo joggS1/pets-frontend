9be0fb1d601988adfe6f80f36fdfc02829a6505c
Nikita Lukin <m.old.ost2@gmail.com>
2025-02-23 16:16:05 +0300

    123

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="text-center !h-full">
-      <h1 className="text-bold text-3xl mb-5">Main page</h1>
+      <h1 className="text-bold text-3xl mb-5">Main page 123</h1>
       <Button size="sm" asChild><Link href="about">About Page</Link></Button>
     </div>
   );
